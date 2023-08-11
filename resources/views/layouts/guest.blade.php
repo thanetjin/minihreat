3212ad26ea85596318eb0810dcbf736e24cf8e83
Chayapol <chaya@LAPTOP-OV1E4S87.localdomain>
2023-08-11 21:23:47 +0700

    change register and login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,7 +21,7 @@
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a> -->
 
-                <img src="http://localhost/images/miniheart.jpg"  alt="Logo" width="110" height="600">
+                <img src="{{ URL('images/miniheart.jpg') }}"  alt="Logo" width="110" height="600">
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">

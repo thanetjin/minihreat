7c371a2723d4cdc241f630ec86602f8bead335e6
kornavit <kornavit.p@ku.th>
2023-08-11 18:53:31 +0700

    merge admin seeder and database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,12 @@
     Welcome to Laravel!!
     <div>
         
-            <h1>{{ $event->event_name }}</h1>
+            {{-- <h1>{{ $event->event_name }}</h1>
             @foreach ($event->users as $user)
             <span>{{ $user->name }}</span>
                 
             @endforeach
+            --}}
         
     </div>
 </h1>

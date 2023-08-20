d3b0e5d3d900e131f3f8952bcba1dead3a4fdd52
kornavit <kornavit.p@ku.th>
2023-08-20 11:19:41 +0700

    delete icon

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -17,14 +17,7 @@
                     <img class="w-[97px] h-[95px] mx-auto rounded-full" src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="default image">
                 @endif
             <div class="flex justify-center my-2">
-                {{-- <p class="text-center text-2xl">{{ Auth::user()->name}}</p> --}}
                 <p class="text-center text-2xl">{{$user->name}}</p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none" class="mt-2 ml-1">
-                    <path d="M12.4525 6.81852L12.448 6.82482L6.02434 13.2491L7.94954 15.1736L14.3786 8.74481L12.4525 6.81852Z" fill="#1F2A37"/>
-                    <path d="M2.82559 10.05L4.75078 11.9763L11.1753 5.55203L11.1816 5.54753L9.2546 3.62034L2.82559 10.05Z" fill="#1F2A37"/>
-                    <path d="M1.79594 11.5658L0.0462492 16.8145C-0.0617561 17.1377 0.0228479 17.495 0.26406 17.7354C0.435068 17.9073 0.66548 18 0.900391 18C0.995796 18 1.0921 17.9847 1.18481 17.9532L6.43297 16.2033L1.79594 11.5658Z" fill="#1F2A37"/>
-                    <path d="M16.9401 1.05856C15.5279 -0.352853 13.2292 -0.352853 11.817 1.05856L10.5282 2.34755L15.6521 7.47202L16.941 6.18303C18.3532 4.77072 18.3532 2.47087 16.9401 1.05856Z" fill="#1F2A37"/>
-                </svg>
             </div>
             <div class="block p-6 bg-white border border-black rounded-lg">
                 <p class="h-[5vh]">ประวัติส่วนตัว</p>
